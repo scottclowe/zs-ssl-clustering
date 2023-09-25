c4dc58852a6b102b838deef77d821f3ee5f9b7eb
JoakimBHaurum <joakim.haurum@gmail.com>
2023-09-25 14:58:59 +0200

    fixed formatting

--- zs_ssl_clustering/moco/moco.py
+++ zs_ssl_clustering/moco/moco.py
@@ -1,20 +1,24 @@
 import os
+
 import torch
 import torchvision.models as torchvision_models
+
 from zs_ssl_clustering.moco import vits
 
-model_name_to_weights = {"resnet50": "r-50-1000ep.pth.tar",
-                         "vit_small": "vit-s-300ep.pth.tar",
-                         "vit_base": "vit-b-300ep.pth.tar"}
+model_name_to_weights = {
+    "resnet50": "r-50-1000ep.pth.tar",
+    "vit_small": "vit-s-300ep.pth.tar",
+    "vit_base": "vit-b-300ep.pth.tar",
+}
+
 
 def load_pretrained_model(model_name, pretrained_dir):
-
-    if model_name.startswith('vit'):
+    if model_name.startswith("vit"):
         model = vits.__dict__[model_name]()
-        linear_keyword = 'head'
+        linear_keyword = "head"
     else:
         model = torchvision_models.__dict__[model_name]()
-        linear_keyword = 'fc'
+        linear_keyword = "fc"
 
     weight_path = os.path.join(pretrained_dir, model_name_to_weights[model_name])
     assert os.path.isfile(weight_path)
@@ -23,17 +27,22 @@ def load_pretrained_model(model_name, pretrained_dir):
     checkpoint = torch.load(weight_path, map_location="cpu")
 
     # rename moco pre-trained keys
-    state_dict = checkpoint['state_dict']
+    state_dict = checkpoint["state_dict"]
     for k in list(state_dict.keys()):
         # retain only base_encoder up to before the embedding layer
-        if k.startswith('module.base_encoder') and not k.startswith('module.base_encoder.%s' % linear_keyword):
+        if k.startswith("module.base_encoder") and not k.startswith(
+            "module.base_encoder.%s" % linear_keyword
+        ):
             # remove prefix
-            state_dict[k[len("module.base_encoder."):]] = state_dict[k]
+            state_dict[k[len("module.base_encoder.") :]] = state_dict[k]
         # delete renamed or unused k
         del state_dict[k]
 
     msg = model.load_state_dict(state_dict, strict=False)
-    assert set(msg.missing_keys) == {"%s.weight" % linear_keyword, "%s.bias" % linear_keyword}
+    assert set(msg.missing_keys) == {
+        "%s.weight" % linear_keyword,
+        "%s.bias" % linear_keyword,
+    }
 
     print("=> loaded pre-trained model '{}'".format(weight_path))
 
--- zs_ssl_clustering/moco/vits.py
+++ zs_ssl_clustering/moco/vits.py
@@ -5,20 +5,20 @@
 # LICENSE file in the root directory of this source tree.
 
 import math
-import torch
-import torch.nn as nn
 from functools import partial, reduce
 from operator import mul
 
-from timm.models.vision_transformer import VisionTransformer, _cfg
-from timm.layers.helpers import to_2tuple
+import torch
+import torch.nn as nn
 from timm.layers import PatchEmbed
+from timm.layers.helpers import to_2tuple
+from timm.models.vision_transformer import VisionTransformer, _cfg
 
 __all__ = [
-    'vit_small', 
-    'vit_base',
-    'vit_conv_small',
-    'vit_conv_base',
+    "vit_small",
+    "vit_base",
+    "vit_conv_small",
+    "vit_conv_base",
 ]
 
 
@@ -31,9 +31,11 @@ class VisionTransformerMoCo(VisionTransformer):
         # weight initialization
         for name, m in self.named_modules():
             if isinstance(m, nn.Linear):
-                if 'qkv' in name:
+                if "qkv" in name:
                     # treat the weights of Q, K, V separately
-                    val = math.sqrt(6. / float(m.weight.shape[0] // 3 + m.weight.shape[1]))
+                    val = math.sqrt(
+                        6.0 / float(m.weight.shape[0] // 3 + m.weight.shape[1])
+                    )
                     nn.init.uniform_(m.weight, -val, val)
                 else:
                     nn.init.xavier_uniform_(m.weight)
@@ -42,7 +44,12 @@ class VisionTransformerMoCo(VisionTransformer):
 
         if isinstance(self.patch_embed, PatchEmbed):
             # xavier_uniform initialization
-            val = math.sqrt(6. / float(3 * reduce(mul, self.patch_embed.patch_size, 1) + self.embed_dim))
+            val = math.sqrt(
+                6.0
+                / float(
+                    3 * reduce(mul, self.patch_embed.patch_size, 1) + self.embed_dim
+                )
+            )
             nn.init.uniform_(self.patch_embed.proj.weight, -val, val)
             nn.init.zeros_(self.patch_embed.proj.bias)
 
@@ -50,34 +57,48 @@ class VisionTransformerMoCo(VisionTransformer):
                 self.patch_embed.proj.weight.requires_grad = False
                 self.patch_embed.proj.bias.requires_grad = False
 
-    def build_2d_sincos_position_embedding(self, temperature=10000.):
+    def build_2d_sincos_position_embedding(self, temperature=10000.0):
         h, w = self.patch_embed.grid_size
         grid_w = torch.arange(w, dtype=torch.float32)
         grid_h = torch.arange(h, dtype=torch.float32)
         grid_w, grid_h = torch.meshgrid(grid_w, grid_h)
-        assert self.embed_dim % 4 == 0, 'Embed dimension must be divisible by 4 for 2D sin-cos position embedding'
+        assert (
+            self.embed_dim % 4 == 0
+        ), "Embed dimension must be divisible by 4 for 2D sin-cos position embedding"
         pos_dim = self.embed_dim // 4
         omega = torch.arange(pos_dim, dtype=torch.float32) / pos_dim
-        omega = 1. / (temperature**omega)
-        out_w = torch.einsum('m,d->md', [grid_w.flatten(), omega])
-        out_h = torch.einsum('m,d->md', [grid_h.flatten(), omega])
-        pos_emb = torch.cat([torch.sin(out_w), torch.cos(out_w), torch.sin(out_h), torch.cos(out_h)], dim=1)[None, :, :]
+        omega = 1.0 / (temperature**omega)
+        out_w = torch.einsum("m,d->md", [grid_w.flatten(), omega])
+        out_h = torch.einsum("m,d->md", [grid_h.flatten(), omega])
+        pos_emb = torch.cat(
+            [torch.sin(out_w), torch.cos(out_w), torch.sin(out_h), torch.cos(out_h)],
+            dim=1,
+        )[None, :, :]
 
-        assert self.num_prefix_tokens == 1, 'Assuming one and only one token, [cls]'
+        assert self.num_prefix_tokens == 1, "Assuming one and only one token, [cls]"
         pe_token = torch.zeros([1, 1, self.embed_dim], dtype=torch.float32)
         self.pos_embed = nn.Parameter(torch.cat([pe_token, pos_emb], dim=1))
         self.pos_embed.requires_grad = False
 
 
 class ConvStem(nn.Module):
-    """ 
+    """
     ConvStem, from Early Convolutions Help Transformers See Better, Tete et al. https://arxiv.org/abs/2106.14881
     """
-    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768, norm_layer=None, flatten=True):
+
+    def __init__(
+        self,
+        img_size=224,
+        patch_size=16,
+        in_chans=3,
+        embed_dim=768,
+        norm_layer=None,
+        flatten=True,
+    ):
         super().__init__()
 
-        assert patch_size == 16, 'ConvStem only supports patch size of 16'
-        assert embed_dim % 8 == 0, 'Embed dimension must be divisible by 8 for ConvStem'
+        assert patch_size == 16, "ConvStem only supports patch size of 16"
+        assert embed_dim % 8 == 0, "Embed dimension must be divisible by 8 for ConvStem"
 
         img_size = to_2tuple(img_size)
         patch_size = to_2tuple(patch_size)
@@ -90,8 +111,17 @@ class ConvStem(nn.Module):
         # build stem, similar to the design in https://arxiv.org/abs/2106.14881
         stem = []
         input_dim, output_dim = 3, embed_dim // 8
-        for l in range(4):
-            stem.append(nn.Conv2d(input_dim, output_dim, kernel_size=3, stride=2, padding=1, bias=False))
+        for _ in range(4):
+            stem.append(
+                nn.Conv2d(
+                    input_dim,
+                    output_dim,
+                    kernel_size=3,
+                    stride=2,
+                    padding=1,
+                    bias=False,
+                )
+            )
             stem.append(nn.BatchNorm2d(output_dim))
             stem.append(nn.ReLU(inplace=True))
             input_dim = output_dim
@@ -103,8 +133,9 @@ class ConvStem(nn.Module):
 
     def forward(self, x):
         B, C, H, W = x.shape
-        assert H == self.img_size[0] and W == self.img_size[1], \
-            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        assert (
+            H == self.img_size[0] and W == self.img_size[1]
+        ), f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
         x = self.proj(x)
         if self.flatten:
             x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
@@ -114,32 +145,63 @@ class ConvStem(nn.Module):
 
 def vit_small(**kwargs):
     model = VisionTransformerMoCo(
-        patch_size=16, embed_dim=384, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+        patch_size=16,
+        embed_dim=384,
+        depth=12,
+        num_heads=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        **kwargs,
+    )
     model.default_cfg = _cfg()
     return model
 
+
 def vit_base(**kwargs):
     model = VisionTransformerMoCo(
-        patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+        patch_size=16,
+        embed_dim=768,
+        depth=12,
+        num_heads=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        **kwargs,
+    )
     model.default_cfg = _cfg()
     return model
+
 
 def vit_conv_small(**kwargs):
     # minus one ViT block
     model = VisionTransformerMoCo(
-        patch_size=16, embed_dim=384, depth=11, num_heads=12, mlp_ratio=4, qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6), embed_layer=ConvStem, **kwargs)
+        patch_size=16,
+        embed_dim=384,
+        depth=11,
+        num_heads=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        embed_layer=ConvStem,
+        **kwargs,
+    )
     model.default_cfg = _cfg()
     return model
+
 
 def vit_conv_base(**kwargs):
     # minus one ViT block
     model = VisionTransformerMoCo(
-        patch_size=16, embed_dim=768, depth=11, num_heads=12, mlp_ratio=4, qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6), embed_layer=ConvStem, **kwargs)
+        patch_size=16,
+        embed_dim=768,
+        depth=11,
+        num_heads=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        embed_layer=ConvStem,
+        **kwargs,
+    )
     model.default_cfg = _cfg()
     return model
-
-
