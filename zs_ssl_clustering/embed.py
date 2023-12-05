scottclowe/zs-ssl-clustering

commit 35f0f90467aed1acea5d003bb7e2cc1db43f81f1

MNT: Simplify embedding code by removing superfluous DDP option

zs_ssl_clustering/embed.py
@@ -9,72 +9,28 @@ import numpy as np
 import torch
 import torch.optim
 import torch.utils.data
-from torch import nn
-from torch.utils.data.distributed import DistributedSampler
 
 from zs_ssl_clustering import data_transformations, datasets, encoders, io, utils
 
 
 def run(config):
     r"""
-    Begin running the experiment.
+    Begin running the experiment on a single worker.
 
     Parameters
     ----------
     config : argparse.Namespace or OmegaConf
         The configuration for this experiment.
     """
-    ngpus_per_node = torch.cuda.device_count()
-    config.world_size = ngpus_per_node * config.node_count
-    config.distributed = config.world_size > 1
-    config.batch_size = config.batch_size_per_gpu * config.world_size
-
-    if config.distributed:
-        # Use torch.multiprocessing.spawn to launch distributed processes: the
-        # run_one_worker process function
-        torch.multiprocessing.spawn(
-            run_one_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, config)
-        )
-    else:
-        # Simply call main_worker function once
-        run_one_worker(config.gpu, ngpus_per_node, config)
-
-
-def run_one_worker(gpu, ngpus_per_node, config):
-    r"""
-    Run one worker in the distributed training process.
-
-    Parameters
-    ----------
-    gpu : int
-        The GPU index of this worker, relative to this node.
-    ngpus_per_node : int
-        The number of GPUs per node.
-    config : argparse.Namespace or OmegaConf
-        The configuration for this experiment.
-    """
-    config.gpu = gpu
+    config.batch_size_per_gpu = config.batch_size
 
     if config.seed is not None:
         utils.set_rng_seeds_fixed(config.seed)
-    elif config.distributed and not config.model_output_dir and not config.models_dir:
-        raise ValueError(
-            "A seed or checkpoint file must be specified for distributed training"
-            " so that each GPU-worker starts with the same initial weights."
-        )
 
     if config.deterministic:
         print("Running in deterministic cuDNN mode. Performance may be slower.")
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
-
-    # Suppress printing if this is not the master process for the node
-    if config.distributed and config.gpu != 0:
-
-        def print_pass(*args, **kwargs):
-            pass
-
-        builtins.print = print_pass
 
     n_cpus = utils.get_num_cpu_available()
     print()
@@ -82,31 +38,11 @@ def run_one_worker(gpu, ngpus_per_node, config):
     print()
     print(config)
     print()
-    print(f"Node rank {config.node_rank}")
     print(f"Found {torch.cuda.device_count()} GPUs and {n_cpus} CPUs.")
 
     if config.log_wandb:
         # Lazy import of wandb, since logging to wandb is optional
         import wandb
-
-    # DISTRIBUTION ============================================================
-    if config.distributed:
-        # For multiprocessing distributed training, gpu rank needs to be
-        # set to the global rank among all the processes.
-        config.gpu_rank = config.node_rank * ngpus_per_node + gpu
-        print(f"GPU rank {config.gpu_rank} of {config.world_size}")
-        print(
-            f"Communicating with master worker {config.dist_url} via {config.dist_backend}"
-        )
-        torch.distributed.init_process_group(
-            backend=config.dist_backend,
-            init_method=config.dist_url,
-            world_size=config.world_size,
-            rank=config.gpu_rank,
-        )
-        torch.distributed.barrier()
-    else:
-        config.gpu_rank = 0
 
     # Check which device to use
     use_cuda = not config.no_cuda and torch.cuda.is_available()
@@ -143,21 +79,6 @@ def run_one_worker(gpu, ngpus_per_node, config):
 
     if not torch.cuda.is_available():
         print("Using CPU (this will be slow)")
-    elif config.distributed:
-        # For multiprocessing distributed, the DistributedDataParallel
-        # constructor should always set a single device scope, otherwise
-        # DistributedDataParallel will use all available devices.
-        encoder.to(device)
-        if config.gpu is not None:
-            torch.cuda.set_device(config.gpu)
-            config.workers = int((config.workers + ngpus_per_node - 1) / ngpus_per_node)
-            encoder = nn.parallel.DistributedDataParallel(
-                encoder, device_ids=[config.gpu]
-            )
-        else:
-            # DistributedDataParallel will divide and allocate batch_size to all
-            # available GPUs if device_ids are not set
-            encoder = nn.parallel.DistributedDataParallel(encoder)
     else:
         if config.gpu is not None:
             torch.cuda.set_device(config.gpu)
@@ -219,15 +140,6 @@ def run_one_worker(gpu, ngpus_per_node, config):
         cuda_kwargs = {"num_workers": config.workers, "pin_memory": True}
         dl_kwargs.update(cuda_kwargs)
 
-    if config.distributed:
-        # The DistributedSampler breaks up the dataset across the GPUs
-        dl_kwargs["sampler"] = DistributedSampler(
-            dataset,
-            shuffle=False,
-            drop_last=False,
-        )
-        dl_kwargs["shuffle"] = None
-
     dataloader = torch.utils.data.DataLoader(dataset, **dl_kwargs)
 
     # TRAIN ===================================================================
@@ -242,9 +154,7 @@ def run_one_worker(gpu, ngpus_per_node, config):
     # Create embeddings
     t0 = time.time()
     print("Creating embeddings...")
-    embeddings, y_true = embed_dataset(
-        dataloader, encoder, device, is_distributed=config.distributed
-    )
+    embeddings, y_true = embed_dataset(dataloader, encoder, device)
     print(f"Created {len(embeddings)} embeddings in {time.time() - t0:.2f}s")
     fname = io.get_embeddings_path(config)
     # Save embeddings
@@ -435,13 +345,9 @@ def get_parser():
     group = parser.add_argument_group("Hardware configuration")
     group.add_argument(
         "--batch-size",
-        dest="batch_size_per_gpu",
         type=int,
         default=128,
-        help=(
-            "Batch size per GPU. The total batch size will be this value times"
-            " the total number of GPUs used. Default: %(default)s"
-        ),
+        help="Batch size. Default: %(default)s",
     )
     group.add_argument(
         "--workers",
@@ -457,37 +363,7 @@ def get_parser():
         "--gpu",
         default=None,
         type=int,
-        help="Index of GPU to use. Setting this will disable GPU parallelism.",
-    )
-    group.add_argument(
-        "--node-count",
-        default=1,
-        type=int,
-        help="Number of nodes for distributed training.",
-    )
-    group.add_argument(
-        "--node-rank",
-        "--rank",
-        dest="node_rank",
-        default=0,
-        type=int,
-        help="Node rank for distributed training.",
-    )
-    group.add_argument(
-        "--dist-url",
-        default="tcp://localhost:23456",
-        type=str,
-        help="URL used to set up distributed training.",
-    )
-    group.add_argument(
-        "--dist-backend",
-        default="nccl",
-        type=str,
-        help=(
-            "Distributed training backend. Must be supported by"
-            " torch.distributed (one of gloo, mpi, nccl), and supported by"
-            " your GPU server."
-        ),
+        help="Index of GPU to use.",
     )
     # Logging args ------------------------------------------------------------
     group = parser.add_argument_group("Debugging and logging")

slurm/embed.slrm
@@ -99,123 +99,25 @@ echo ""
 echo "-------- Begin main script ---------------------------------------------"
 date
 echo ""
-# Store the master node's IP address in the MASTER_ADDR environment variable.
-export MASTER_ADDR="$(hostname -s)"
-echo "Rank $SLURM_NODEID master node is at $MASTER_ADDR"
-
-# Get the address of a random available socket (aka port number).
-# By briefly opening the connection, the system is less likely to allocate
-# something else to the socket before we start using it for real.
-# https://unix.stackexchange.com/a/132524/
-MASTER_PORT="$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1])')";
-echo "Will use port $MASTER_PORT for NCCL communication"
-
 # Our job requests a minimum number of CPUs per node, but we might have been
 # allocated more than that. ${SLURM_CPUS_ON_NODE} reflects the number of CPUs
 # actually allocated to this job. For reproducibility, we should only use
 # as many workers as the number of CPUs as we requested.
 NUM_WORKERS=$(( SLURM_GPUS_ON_NODE * SLURM_CPUS_PER_GPU ))
 
-if [[ "$SLURM_JOB_NUM_NODES" == "1" ]];
-then
-    # Single node configuration
-    # This is simpler than the multi-GPU configuration, as we only have to
-    # launch one embed.py process.
-    echo ""
-    echo "Main script begins with host tcp://localhost:$MASTER_PORT with backend NCCL"
-    echo "Single ($SLURM_JOB_NUM_NODES) node training ($SLURM_GPUS_ON_NODE GPUs)"
-    echo ""
-    #
-    # We just need to call the embed.py script once and embed.py will launch a
-    # process for each GPU on that node automatically.
-    #
-    # We pass on to embed.py an arary of arbitrary extra arguments given to this
-    # slurm script contained in the $@ magic variable (i.e. "${@}")
-    #
-    # N.B. If you alter this, be sure to alter the multi-GPU implementation
-    # below too!
-    python "$PROJECT_DIRN/embed.py" \
-        --rank=0 \
-        --dist-url="tcp://localhost:$MASTER_PORT" \
-        --node-count="$SLURM_JOB_NUM_NODES" \
-        --workers="$NUM_WORKERS" \
-        --seed="$SEED" \
-        --log-wandb \
-        --run-name="$SLURM_JOB_NAME" \
-        --run-id="$JOB_ID" \
-        "${@}"
-
-else
-    # Multi-GPU configuration
-    echo ""
-    echo "Main script begins with host tcp://${MASTER_ADDR}:$MASTER_PORT with backend NCCL"
-    echo "Multiple ($SLURM_JOB_NUM_NODES) node training (x$SLURM_GPUS_ON_NODE GPUs per node)"
-    echo ""
-    # export NCCL_DEBUG=INFO  # Enable this to debug NCCL errors
-    export NCCL_BLOCKING_WAIT=1  # Set this when using the NCCL backend for inter-GPU communication.
-    export NCCL_IB_DISABLE=1
-    #
-    # Make a shell script that calls our main.py script. The shell script
-    # will have variables written to it from this process, but will use its
-    # own SLURM environment variable to determine its rank.
-    # We do this by defining and then calling a worker_command fuction so we can
-    # more easily decide where to pipe its output. The main worker has its
-    # output piped to the main output file, whilst other workers pipe their
-    # outputs to their own files in the slogs-inner directory.
-    #
-    # N.B. If you alter the command to write the script below, be sure to alter
-    # the single-GPU implementation above too!
-    mkdir -p slrm_auto_script
-    worker_script="slrm_auto_script/srun_worker_${JOB_LABEL}.sh"
-    if [ "$SBATCH_OUTPUT" != "" ];
-    then
-        :  # pass
-    elif [ "$SLURM_ARRAY_TASK_COUNT" != "" ];
-    then
-        SBATCH_OUTPUT="slogs/${SLURM_JOB_NAME}__${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.out";
-    else
-        SBATCH_OUTPUT="slogs/${SLURM_JOB_NAME}__${SLURM_JOB_ID}_4294967294.out";
-    fi
-    OUTPUT_BASE="${SBATCH_OUTPUT##*/}"
-    OUTPUT_STEM="${OUTPUT_BASE%.*}"
-    OUTPUT_EXTN="${OUTPUT_BASE##*.}"
-    cat > "$worker_script" <<- EOM
-sleep \$(( SLURM_PROCID * 5 ))
-worker_command() {
-    python $PROJECT_DIRN/embed.py \
---rank="\${SLURM_PROCID}" \
---dist-url="tcp://${MASTER_ADDR}:${MASTER_PORT}" \
---node-count="$SLURM_JOB_NUM_NODES" \
---workers="$NUM_WORKERS" \
---seed="$SEED" \
---log-wandb \
---run-name="$SLURM_JOB_NAME" \
---run-id="$JOB_ID" \
-${@}
-}
-OUTPUT_FNAME="slogs-inner/${OUTPUT_STEM}-r\${SLURM_PROCID}.${OUTPUT_EXTN}"
-if (( SLURM_PROCID  == 0 )); then
-    worker_command | tee -a "\$OUTPUT_FNAME"
-else
-    worker_command >> "\$OUTPUT_FNAME"
-fi
-EOM
-    # Create a log directory for our inner tasks to write their outputs to.
-    mkdir -p slogs-inner
-    echo ""
-    echo "Will execute $worker_script on $SLURM_JOB_NUM_NODES nodes, with contents:"
-    cat "$worker_script"
-    echo ""
-    # By executing srun inside a slurm script, we initiate one srun instance
-    # for every task we created in the SLURM header. Since we asked for one
-    # task per node, each of these will run this command. The main worker
-    # wrote the srun_worker_JOBLABEL.sh script and fixed the variables that
-    # the other nodes need to know so they can communicate with each other.
-    # Once embed.py is run on each node, it will handle spawning one process
-    # for each of the GPUs on the node.
-    srun --nodes="$SLURM_JOB_NUM_NODES" bash "$worker_script"
-
-fi
+echo ""
+echo "Main script begins."
+echo "Single GPU job ($SLURM_JOB_NUM_NODES x $SLURM_GPUS_ON_NODE)"
+echo ""
+# We pass on to embed.py an arary of arbitrary extra arguments given to this
+# slurm script contained in the $@ magic variable (i.e. "${@}")
+python "$PROJECT_DIRN/embed.py" \
+    --workers="$NUM_WORKERS" \
+    --seed="$SEED" \
+    --log-wandb \
+    --run-name="$SLURM_JOB_NAME" \
+    --run-id="$JOB_ID" \
+    "${@}"
 
 echo ""
 echo "------------------------------------------------------------------------"

commit 030867dae048d97f2fb028c1b622b47c5faa4448

BUG: Need to always save embeddings now DDP is removed
Fixes a bug introduced in 35f0f90, with a residual reference to
gpu_rank which is no longer declared as a config argument.

zs_ssl_clustering/embed.py
@@ -99,20 +99,19 @@ def run(config):
     # Save --------------------------------------------------------------------
     fname = io.get_embeddings_path(config)
     # Save embeddings
-    if config.gpu_rank == 0:
-        os.makedirs(os.path.dirname(fname), exist_ok=True)
-        print(f"Saving embeddings to {fname}")
-        t1 = time.time()
-        tmp_a, tmp_b = os.path.split(fname)
-        tmp_fname = os.path.join(tmp_a, ".tmp." + tmp_b)
-        np.savez_compressed(
-            tmp_fname,
-            config=config,
-            embeddings=embeddings,
-            y_true=y_true,
-        )
-        os.rename(tmp_fname, fname)
-        print(f"Saved embeddings in {time.time() - t1:.2f}s")
+    os.makedirs(os.path.dirname(fname), exist_ok=True)
+    print(f"Saving embeddings to {fname}")
+    t1 = time.time()
+    tmp_a, tmp_b = os.path.split(fname)
+    tmp_fname = os.path.join(tmp_a, ".tmp." + tmp_b)
+    np.savez_compressed(
+        tmp_fname,
+        config=config,
+        embeddings=embeddings,
+        y_true=y_true,
+    )
+    os.rename(tmp_fname, fname)
+    print(f"Saved embeddings in {time.time() - t1:.2f}s")
 
 
 def make_dataloader(config, use_cuda=False):