50b0d6560ced3c584edbd91c3021231a7465c42e
Scott Lowe <scott.code.lowe@gmail.com>
2024-01-24 15:54:33 -0500

    BUG: Fix loading MAE with global pooling enabled in timm==0.9.12

--- zs_ssl_clustering/encoders/mae/models_vit.py
+++ zs_ssl_clustering/encoders/mae/models_vit.py
@@ -12,8 +12,8 @@ Adapted from
 # The original source code can be found at
 # https://github.com/facebookresearch/mae/blob/efb2a80/models_vit.py
 #
-# Modifications from the original are indicated by comments (whitespace and
-# formatting changes are not indicated).
+# The implementation was modified to work with timm==0.9.12, preserving the
+# behaviour of the original code that was designed with timm==0.3.2 and timm==0.4.5.
 
 # --------------------------------------------------------
 # References:
@@ -24,51 +24,12 @@ Adapted from
 from functools import partial
 
 import timm.models.vision_transformer
-import torch
 import torch.nn as nn
 
 
-class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
-    """Vision Transformer with support for global average pooling"""
-
-    def __init__(self, global_pool=False, **kwargs):
-        super(VisionTransformer, self).__init__(**kwargs)
-
-        self.global_pool = global_pool
-        if self.global_pool:
-            norm_layer = kwargs["norm_layer"]
-            embed_dim = kwargs["embed_dim"]
-            self.fc_norm = norm_layer(embed_dim)
-
-            del self.norm  # remove the original norm
-
-    def forward_features(self, x):
-        B = x.shape[0]
-        x = self.patch_embed(x)
-
-        cls_tokens = self.cls_token.expand(
-            B, -1, -1
-        )  # stole cls_tokens impl from Phil Wang, thanks
-        x = torch.cat((cls_tokens, x), dim=1)
-        x = x + self.pos_embed
-        x = self.pos_drop(x)
-
-        for blk in self.blocks:
-            x = blk(x)
-
-        if self.global_pool:
-            # Changed: Added keepdim=True
-            x = x[:, 1:, :].mean(dim=1, keepdim=True)  # global pool without cls token
-            outcome = self.fc_norm(x)
-        else:
-            x = self.norm(x)
-            outcome = x[:, 0]
-
-        return outcome
-
-
-def vit_base_patch16(**kwargs):
-    model = VisionTransformer(
+def vit_base_patch16(global_pool=False, **kwargs):
+    timm_global_pool = "avg" if global_pool else "token"
+    model = timm.models.vision_transformer.VisionTransformer(
         patch_size=16,
         embed_dim=768,
         depth=12,
@@ -76,13 +37,15 @@ def vit_base_patch16(**kwargs):
         mlp_ratio=4,
         qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        global_pool=timm_global_pool,
         **kwargs,
     )
     return model
 
 
-def vit_large_patch16(**kwargs):
-    model = VisionTransformer(
+def vit_large_patch16(global_pool=False, **kwargs):
+    timm_global_pool = "avg" if global_pool else "token"
+    model = timm.models.vision_transformer.VisionTransformer(
         patch_size=16,
         embed_dim=1024,
         depth=24,
@@ -90,13 +53,15 @@ def vit_large_patch16(**kwargs):
         mlp_ratio=4,
         qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        global_pool=timm_global_pool,
         **kwargs,
     )
     return model
 
 
-def vit_huge_patch14(**kwargs):
-    model = VisionTransformer(
+def vit_huge_patch14(global_pool=False, **kwargs):
+    timm_global_pool = "avg" if global_pool else "token"
+    model = timm.models.vision_transformer.VisionTransformer(
         patch_size=14,
         embed_dim=1280,
         depth=32,
@@ -104,6 +69,7 @@ def vit_huge_patch14(**kwargs):
         mlp_ratio=4,
         qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        global_pool=timm_global_pool,
         **kwargs,
     )
     return model
